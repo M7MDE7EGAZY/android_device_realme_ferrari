1548e5b2de95e61cf288b97e2360a4e9f05a0d11
M7MDE7EGAZY <76889809+M7MDE7EGAZY@users.noreply.github.com>
2025-03-06 09:36:01 +0200

    Update extract-files.py

--- extract-files.py
+++ extract-files.py
@@ -51,7 +51,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'ferrari',
+    'ferrarri',
     'realme',
     namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
