16967579a381d20f86e70b866d34b03a623c2905
M7MDE7EGAZY <76889809+M7MDE7EGAZY@users.noreply.github.com>
2025-03-06 09:32:42 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,7 @@
 # Include the common OEM chipset BoardConfig.
 include device/oneplus/sm8450-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/realme/ferrari
+DEVICE_PATH := device/realme/ferrarri
 
 # DTB
 TARGET_KERNEL_CONFIG += vendor/oplus/ferrari.config
@@ -23,4 +23,4 @@ TARGET_RECOVERY_DENSITY := xxhdpi
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 126
 
 # Include the proprietary files BoardConfig.
-include vendor/realme/ferrari/BoardConfigVendor.mk
+include vendor/realme/ferrarri/BoardConfigVendor.mk
